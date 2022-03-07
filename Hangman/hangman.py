45b9dc26d74502a079b29cd7791f074ce17900b1
DVS-PLATTLING\kgreiner <kevin.greiner@edvschule-plattling.de>
2022-03-07 08:53:16 +0100

    Bilder eingefügt

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -7,7 +7,20 @@ window.maxsize(650,400)
 window.geometry("550x300")
 window.title("Hangman")
 
+arme = tk.PhotoImage(file="Assets/Arme.gif")
+beine = tk.PhotoImage(file="Assets/Beine.gif")
+kopf = tk.PhotoImage(file="Assets/Kopf.gif")
+koerper = tk.PhotoImage(file="Assets/Körper.gif")
+pfahl = tk.PhotoImage(file="Assets/Pfahl.gif")
+seil = tk.PhotoImage(file="Assets/Seil.gif")
+
+lbl_ueb = tk.Label(window, text="Hangman", font=("Comic Sans MS", 30, "bold underline"))
 
 
+
+
+
+#grid
+lbl_ueb.grid(row=0, column=1,padx=(240,0), pady=(5,0))
 
 window.mainloop()
