8712bb918187d93d848c5f0b2a2d623d3d74e63a
DVS-PLATTLING\kgreiner <kevin.greiner@edvschule-plattling.de>
2022-03-07 09:29:06 +0100

    Button noch ins grid rein

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -17,12 +17,12 @@ seil = tk.PhotoImage(file="Assets/Seil.gif")
 #Überschrift
 lbl_ueb = tk.Label(window, text="Hangman", font=("Comic Sans MS", 30, "bold underline"))
 #Button zum Starten des Spiels
-btn_play = tk.Button(window, text="Spielen")
+btn_play = tk.Button(window, text="Spielen", font=("Comic Sans MS", 20, "bold"))
 
 
 
 #grid
 lbl_ueb.grid(row=0, column=0,padx=(240,0), pady=(5,0))
-
+btn_play.grid(row=1, column=0, padx=(240,0), pady=(100, 0))
 
 window.mainloop()
